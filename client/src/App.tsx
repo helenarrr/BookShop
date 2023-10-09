8ed21e7dc5dd44b635280447f88cd32b750ab126
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-10-09 23:15:20 +0300

    commit 07 Встраивание HomePage в App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from './layouts/NavigationAndFooter/NavigationBar';
+import HomePage from './layouts/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-
       <NavigationBar />
+      <HomePage />
     </div>
   );
 }
