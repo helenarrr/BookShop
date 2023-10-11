54e58f3fbda7c8502cc30227321dea6e9318e221
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-10-11 16:07:52 +0300

    commit 013 Внедрение компонентов в HomePage

--- client/src/layouts/HomePage/HomePage.tsx
+++ client/src/layouts/HomePage/HomePage.tsx
@@ -1,9 +1,15 @@
+import CallAction from "./components/CallAction";
+import Carousel from "./components/Carousel";
 import ExploreBooks from "./components/ExploreBooks";
+import LibraryServices from "./components/LibraryServices";
 
 function HomePage() {
     return (
         <div>
             <ExploreBooks />
+            <Carousel />
+            <CallAction />
+            <LibraryServices />
         </div>
     );
 }
