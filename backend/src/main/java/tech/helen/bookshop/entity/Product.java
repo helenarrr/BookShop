b63a93a11ae08d2acf526ad758bb506aa85f8cfb
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-10-11 23:10:29 +0300

    commit 016 Добавление модели продукта

---
+++ backend/src/main/java/tech/helen/bookshop/entity/Product.java
@@ -0,0 +1,37 @@
+package tech.helen.bookshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "creator")
+    private String creator;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "copies")
+    private String copies;
+
+    @Column(name = "copies_available")
+    private int copiesAvailable;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "img")
+    private String img;
+
+}
--- client/src/layouts/NavigationAndFooter/Footer.tsx
+++ client/src/layouts/NavigationAndFooter/Footer.tsx
@@ -3,7 +3,7 @@ function Footer() {
         <div className='main-color'>
             <footer className='container d-flex flex-wrap 
                   justify-content-between align-items-center py-5 main-color'>
-                <p className='col-md-4 mb-0 text-white'>© books.ksergei.tech</p>
+                <p className='col-md-4 mb-0 text-white'>© books.elena.tech</p>
                 <ul className='nav navbar-dark col-md-4 justify-content-end'>
                     <li className='nav-item'>
                         <a href='/' className='nav-link px-2 text-white'>
