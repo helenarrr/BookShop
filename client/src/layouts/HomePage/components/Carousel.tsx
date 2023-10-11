9174f325792c2d94866dc9fe23484c9e4f684bf4
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-10-11 15:34:38 +0300

    commit 09 Обновление компонента Carousel

--- client/src/layouts/HomePage/components/Carousel.tsx
+++ client/src/layouts/HomePage/components/Carousel.tsx
@@ -59,6 +59,16 @@ function Carousel() {
                 </button>
             </div>
 
+            <div className="d-lg-none mt-3">
+                <div className="row d-flex justify-content-center align-items-center">
+                    <ReturnBookItem />
+                </div>
+            </div>
+
+            <div className="homepage-carousel-title mt-2">
+                <a className="btn btn-outline-secondary btn-lg" href="/">Подробности</a>
+            </div>
+
         </div>
     );
 }
