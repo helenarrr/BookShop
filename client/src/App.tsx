110669f544764750bfdd0b13ced08ef8f6ff738e
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-10-12 22:28:37 +0300

    commit 024 Внедрение настроек роутинга

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,25 @@ import './App.css';
 import NavigationBar from './layouts/NavigationAndFooter/NavigationBar';
 import HomePage from './layouts/HomePage/HomePage';
 import Footer from './layouts/NavigationAndFooter/Footer';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div className='d-flex flex-column min-vh-100'>
       <NavigationBar />
-      <HomePage />
+
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/home" exact>
+          <HomePage />
+        </Route>
+        <Route path="/search">
+
+        </Route>
+      </Switch>
+
       <Footer />
     </div>
   );
