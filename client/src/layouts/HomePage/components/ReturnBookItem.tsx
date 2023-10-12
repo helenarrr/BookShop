8eab522d1f5e164b6fa9170146ca8ee44c593b75
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-10-12 23:49:00 +0300

    commit 028 Отображение данных, полученных с сервера

--- client/src/layouts/HomePage/components/ReturnBookItem.tsx
+++ client/src/layouts/HomePage/components/ReturnBookItem.tsx
@@ -1,15 +1,29 @@
-function ReturnBookItem() {
+import Book from "../../../models/Book";
+
+interface Props {
+    book: Book;
+}
+
+function ReturnBookItem({ book }: Props) {
     return (
         <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
             <div className="text-center">
-                <img
-                    className="image-carousel carousel-img"
-                    src={require('./../../../Images/ImagesBooks/book_001.png')}
-                    alt=""
-                />
-                <h6>Книга</h6>
-                <p>Наименование</p>
-                <a className="btn main-color text-white" href="/">Текст 1</a>
+                {
+                    book.img ? <img
+                        className="image-carousel carousel-img"
+                        src={book.img}
+                        alt=""
+                    />
+                        :
+                        <img
+                            className="image-carousel carousel-img"
+                            src={require('./../../../Images/ImagesBooks/book_000.png')}
+                            alt=""
+                        />
+                }
+                <h6>{book.title}</h6>
+                <p>{book.creator}</p>
+                <a className="btn main-color text-white" href="/">Подробнее</a>
             </div>
         </div>
     );
