a6a8c8f4212bcf48fd82827a13ce00dc12419021
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-10-24 14:19:44 +0300

    commit 044 Обновление CORS политик

--- backend/src/main/java/tech/helen/bookshop/config/MethodDataRestConfig.java
+++ backend/src/main/java/tech/helen/bookshop/config/MethodDataRestConfig.java
@@ -7,40 +7,43 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import tech.helen.bookshop.entity.Product;
+import tech.helen.bookshop.entity.Review;
 
 @Configuration
 public class MethodDataRestConfig implements RepositoryRestConfigurer {
 
-    private String clientUrl = Environment.getInstance().getHost();
+        private String clientUrl = Environment.getInstance().getHost();
 
-    @Override
-    public void configureRepositoryRestConfiguration(
-            RepositoryRestConfiguration config,
-            CorsRegistry cors) {
+        @Override
+        public void configureRepositoryRestConfiguration(
+                        RepositoryRestConfiguration config,
+                        CorsRegistry cors) {
 
-        HttpMethod[] unsupportedActions = {
-                // HttpMethod.POST,
-                // HttpMethod.DELETE,
-                // HttpMethod.PUT
-        };
+                HttpMethod[] unsupportedActions = {
+                                // HttpMethod.POST,
+                                // HttpMethod.DELETE,
+                                // HttpMethod.PUT
+                };
 
-        config.exposeIdsFor(Product.class);
+                config.exposeIdsFor(Product.class);
+                config.exposeIdsFor(Review.class);
 
-        disableHttpMethods(Product.class, config, unsupportedActions);
+                disableHttpMethods(Product.class, config, unsupportedActions);
+                disableHttpMethods(Review.class, config, unsupportedActions);
 
-        cors.addMapping(config.getBasePath() + "/**")
-                .allowedOrigins(clientUrl);
-    }
+                cors.addMapping(config.getBasePath() + "/**")
+                                .allowedOrigins(clientUrl);
+        }
 
-    private void disableHttpMethods(
-            Class<?> self,
-            RepositoryRestConfiguration config,
-            HttpMethod[] unsupportedActions) {
-        config.getExposureConfiguration()
-                .forDomainType(self)
-                .withItemExposure(
-                        (metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
-                .withCollectionExposure(
-                        (metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
-    }
+        private void disableHttpMethods(
+                        Class<?> self,
+                        RepositoryRestConfiguration config,
+                        HttpMethod[] unsupportedActions) {
+                config.getExposureConfiguration()
+                                .forDomainType(self)
+                                .withItemExposure(
+                                                (metadata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                                .withCollectionExposure(
+                                                (metadata, httpMethods) -> httpMethods.disable(unsupportedActions));
+        }
 }
