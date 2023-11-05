c1e7f59a853e4501f5905d00e01919dd750877a5
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-11-05 22:54:31 +0300

    commit 054 Обновление для использования auth

--- client/src/layouts/NavigationAndFooter/NavigationBar.tsx
+++ client/src/layouts/NavigationAndFooter/NavigationBar.tsx
@@ -1,39 +1,64 @@
+import { Link, NavLink } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react"
+import SpinnerLoading from "../Utils/SpinnerLoading";
+
 function NavigationBar() {
+    const { oktaAuth, authState } = useOktaAuth();
+
+    if (!authState) {
+        return <SpinnerLoading />
+    }
+
+    const handleLogout = async () => {
+        oktaAuth.revokeAccessToken();
+        oktaAuth.closeSession();
+    }
+
     return (
-        <nav className='navbar navbar-expand-lg navbar-dark main-color py-3'>
-            <div className='container-fluid'>
-                <span className='navbar-brand'>Books App</span>
+        <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
+            <div className="container-fluid">
+                <span className="navbar-brand">Books App</span>
                 <button
-                    className='navbar-toggler'
-                    type='button'
-                    data-bs-toggle='collapse'
-                    data-bs-target='#navbarNavDropdown'
-                    aria-controls='navbarNavDropdown'
-                    aria-expanded='false'
-                    aria-label='Toggle Navigation'
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNavDropdown"
+                    aria-controls="navbarNavDropdown"
+                    aria-expanded="false"
+                    aria-label="Toggle Navigation"
                 >
-                    <span className='navbar-toggler-icon'></span>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
 
-                <div className='collapse navbar-collapse' id='navbarNavDropdown'>
-                    <ul className='navbar-nav'>
-                        <li className='nav-item'>
-                            <a className='nav-link' href='/'>
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/home">
                                 Домашняя
-                            </a>
+                            </NavLink>
                         </li>
-                        <li className='nav-item'>
-                            <a className="nav-link" href="/search">
+                        <li className="nav-item">
+                            <NavLink className="nav-link" to="/search">
                                 Поиск
-                            </a>
+                            </NavLink>
                         </li>
                     </ul>
-                    <ul className='navbar-nav ms-auto'>
-                        <li className='nav-item m-1'>
-                            <a type='button' className='btn btn-outline-light' href='/'>
-                                Вход
-                            </a>
-                        </li>
+                    <ul className="navbar-nav ms-auto">
+                        {!authState.isAuthenticated ?
+                            <li className="nav-item m-1">
+                                <Link type="button" className="btn btn-outline-light" to="/login">
+                                    Вход
+                                </Link>
+                            </li>
+                            :
+                            <li>
+                                <button
+                                    className="btn btn-outline-light"
+                                    onClick={handleLogout}>
+                                    Выход
+                                </button>
+                            </li>
+                        }
                     </ul>
                 </div>
             </div>
