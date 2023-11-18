67d0045e2b2b7379df9dc4e738033694f9ce6dce
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-11-18 22:17:45 +0300

    commit 062 Контроллер для доступа к защищенным маршрутам

---
+++ backend/src/main/java/tech/helen/bookshop/controller/ProductController.java
@@ -0,0 +1,55 @@
+package tech.helen.bookshop.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import tech.helen.bookshop.entity.Product;
+import tech.helen.bookshop.service.ProductService;
+import tech.helen.bookshop.utils.JWTParser;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+  private ProductService productService;
+
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
+
+  private String extractEmail(String token, String string) {
+    String email = JWTParser.jwtExtraction(token, "\"sub\"");
+    if (email != null)
+      return email;
+    return "";
+  }
+
+  @PutMapping("/secure/order")
+  public Product orderProduct(
+      @RequestHeader(value = "Authorization") String token,
+      @RequestParam Long productId) throws Exception {
+    String userEmail = extractEmail(token, "\"sub\"");
+    return productService.orderProduct(userEmail, productId);
+  }
+
+  @GetMapping("/secure/isorder/byuser")
+  public Boolean CustomOrderProductByUser(
+      @RequestHeader(value = "Authorization") String token,
+      @RequestParam Long productId) {
+    String userEmail = extractEmail(token, "\"sub\"");
+    return productService.CustomOrderProductByUser(userEmail, productId);
+  }
+
+  @GetMapping("/secure/currentorder/count")
+  public int currentCustomOrderCount(
+      @RequestHeader(value = "Authorization") String token) {
+    String userEmail = extractEmail(token, "\"sub\"");
+    return productService.currentCustomOrderCount(userEmail);
+  }
+}
