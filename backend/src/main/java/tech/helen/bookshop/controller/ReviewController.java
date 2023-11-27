657b43b21526f686222476fa779a4b5886e58332
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-11-27 23:14:01 +0300

    commit 071 Контроллер для доступа к защищенным Review

---
+++ backend/src/main/java/tech/helen/bookshop/controller/ReviewController.java
@@ -0,0 +1,49 @@
+package tech.helen.bookshop.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import tech.helen.bookshop.config.Environment;
+import tech.helen.bookshop.requestmodels.ReviewRequestModel;
+import tech.helen.bookshop.service.ReviewService;
+import tech.helen.bookshop.utils.JWTParser;
+
+@CrossOrigin(Environment.host)
+@RestController
+@RequestMapping("/api/reviews")
+public class ReviewController {
+
+    private ReviewService reviewService;
+
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    private String extractEmail(String token, String string) {
+        String email = JWTParser.jwtExtraction(token, "\"sub\"");
+        if (email != null)
+            return email;
+        return "";
+    }
+
+    @GetMapping("/secure/user/product")
+    public Boolean reviewProductByUser(
+            @RequestHeader(value = "Authorization") String token,
+            @RequestParam Long productId) throws Exception {
+        String userEmail = extractEmail(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("Ошибка пользователя");
+        }
+        return reviewService.userReviewListed(userEmail, productId);
+    }
+
+    @PostMapping("/secure")
+    public void postReview(
+            @RequestHeader(value = "Authorization") String token,
+            @RequestBody ReviewRequestModel reviewRequest) throws Exception {
+        String userEmail = extractEmail(token, "\"sub\"");
+        if (userEmail == null) {
+            throw new Exception("Ошибка пользователя");
+        }
+        reviewService.postReview(userEmail, reviewRequest);
+    }
+}
