c055be863aab13692a30a01c424f9068e98dd0ba
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-11-28 20:53:03 +0300

    commit 074 Модель отзыва для отправки

---
+++ client/src/models/ReviewRequestModels.ts
@@ -0,0 +1,16 @@
+class ReviewRequestModel {
+    rating: number;
+    productId: number;
+    reviewDescription?: string;
+
+    constructor(
+        rating: number,
+        productId: number,
+        reviewDescription: string) {
+        this.rating = rating;
+        this.productId = productId;
+        this.reviewDescription = reviewDescription;
+    }
+}
+
+export default ReviewRequestModel;
