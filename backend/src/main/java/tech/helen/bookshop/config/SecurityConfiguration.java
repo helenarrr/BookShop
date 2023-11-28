66d093a7f7f0ffaada7b07b09adfbbac66849d2d
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-11-28 20:32:06 +0300

    commit 072 Обновление завсисимостей и библиотек для улучшения работоспособности

--- backend/src/main/java/tech/helen/bookshop/config/SecurityConfiguration.java
+++ backend/src/main/java/tech/helen/bookshop/config/SecurityConfiguration.java
@@ -2,7 +2,6 @@ package tech.helen.bookshop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.accept.ContentNegotiationStrategy;
@@ -16,17 +15,17 @@ public class SecurityConfiguration {
         @Bean
         public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-                http.csrf(csrf -> csrf.disable());
+                http.csrf().disable();
 
-                http.authorizeHttpRequests(configurer -> configurer
-                                .requestMatchers(
+                http.authorizeRequests(configurer -> configurer
+                                .antMatchers(
                                                 "/api/products/secure/**",
                                                 "/api/reviews/secure/**")
                                 .authenticated())
-                                .oauth2ResourceServer(server -> server
-                                                .jwt(Customizer.withDefaults()));
+                                .oauth2ResourceServer()
+                                .jwt();
 
-                http.cors(Customizer.withDefaults());
+                http.cors();
 
                 http.setSharedObject(ContentNegotiationStrategy.class,
                                 new HeaderContentNegotiationStrategy());
--- backend/src/main/java/tech/helen/bookshop/entity/Product.java
+++ backend/src/main/java/tech/helen/bookshop/entity/Product.java
@@ -1,6 +1,12 @@
 package tech.helen.bookshop.entity;
 
-import jakarta.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Entity
