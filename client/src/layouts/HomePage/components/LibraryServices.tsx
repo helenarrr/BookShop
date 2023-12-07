1b95c1f8e8b21d322511537bdbd6b4c541cd710b
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-12-07 21:53:55 +0300

    commit 121 Переход к сообщениям с главной страницы

--- client/src/layouts/HomePage/components/LibraryServices.tsx
+++ client/src/layouts/HomePage/components/LibraryServices.tsx
@@ -1,4 +1,9 @@
+import { useOktaAuth } from "@okta/okta-react/";
+import { Link } from "react-router-dom";
+
 function LibraryServices() {
+    const { authState } = useOktaAuth();
+
     return (
         <div className="container my-5">
             <div className="row p-4 align-items-center border shadow-lg">
@@ -6,13 +11,28 @@ function LibraryServices() {
                     <h1 className="display-4 fw-bold">
                         Не получается что-то отыскать?
                     </h1>
-                    <p className="lead">
-                        Обратитесь в службу поддержки
-                    </p>
-                    <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
-                        <a href="/" type="button" className="btn main-color btn-lg px-4 me-md-2 fw-bold text-white">
-                            Техническая поддержка
-                        </a>
+                    <div>
+                        <p className="lead">
+                            Обратитесь в службу поддержки
+                        </p>
+                        <div className="d-grid gap-2 justify-content-md-start mb-4 mb-lg-3">
+                            {
+                                authState?.isAuthenticated ?
+                                    <Link
+                                        to="/messages"
+                                        type="button"
+                                        className="btn main-color btn-lg px-4 me-md-2 fw-bold text-white">
+                                        Техническая поддержка
+                                    </Link>
+                                    :
+                                    <Link
+                                        to="/login"
+                                        type="button"
+                                        className="btn main-color btn-lg px-4 me-md-2 fw-bold text-white">
+                                        Войти
+                                    </Link>
+                            }
+                        </div>
                     </div>
                 </div>
                 <div className="col-lg-4 offset-lg-1 shadow-lg lost-image"></div>
