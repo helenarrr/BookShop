041997c6efa512a930ccdf0af8d0c4fec5394797
Elena Radchenko <elena.radchenko.9@mail.ru>
2023-12-07 23:01:07 +0300

    commit 130 Обновление репозиториев для удаления данных

--- backend/src/main/java/tech/helen/bookshop/dao/CustomOrderRepository.java
+++ backend/src/main/java/tech/helen/bookshop/dao/CustomOrderRepository.java
@@ -2,6 +2,9 @@ package tech.helen.bookshop.dao;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import tech.helen.bookshop.entity.CustomOrder;
 
@@ -10,4 +13,8 @@ public interface CustomOrderRepository extends JpaRepository<CustomOrder, Long> 
     CustomOrder findByUserEmailAndProductId(String userEmail, Long productId);
 
     List<CustomOrder> findProductByUserEmail(String userEmail);
+
+    @Modifying
+    @Query("delete from Order where product_id in :product_id")
+    void deleteAllByProductId(@Param("product_id") Long productId);
 }
--- backend/src/main/java/tech/helen/bookshop/dao/ReviewRepository.java
+++ backend/src/main/java/tech/helen/bookshop/dao/ReviewRepository.java
@@ -3,6 +3,9 @@ package tech.helen.bookshop.dao;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import tech.helen.bookshop.entity.Review;
@@ -12,5 +15,9 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findByProductId(@RequestParam("productId") Long productId, Pageable pageable);
 
     Review findByUserEmailAndProductId(String userEmail, Long productId);
+
+    @Modifying
+    @Query("delete from Review where product_id in :product_id")
+    void deleteAllByProductId(@Param("product_id") Long productId);
 
 }
